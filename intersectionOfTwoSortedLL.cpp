6b8d768ec7035ee58d9eae59e271f08ecbb73d68
deadperalta <86057977+deadperalta@users.noreply.github.com>
2022-09-05 21:34:37 +0530

    Create intersectionOfTwoSortedLL.cpp

---
+++ intersectionOfTwoSortedLL.cpp
@@ -0,0 +1,112 @@
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    Node *head, *tail;
+    int val;
+    
+    cin>>val;
+    head = tail = new Node(val);
+    
+    while(--size)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+}
+
+Node* findIntersection(Node* head1, Node* head2);
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    cin>> n >> m;
+	    
+	    Node* head1 = inputList(n);
+	    Node* head2 = inputList(m);
+	    
+	    Node* result = findIntersection(head1, head2);
+	    
+	    printList(result);
+	    cout<< endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/* The structure of the Linked list Node is as follows:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+Node* findIntersection(Node* head1, Node* head2)
+{
+    Node* i=head1;
+    Node* j=head2;
+    
+    Node* res=new Node(-1);
+    Node* temp=res;
+    
+    while(i && j)
+    {
+        if(i->data == j->data)
+        {
+            temp->next=new Node(i->data);
+            temp=temp->next;
+            i=i->next;
+            j=j->next;
+        }
+        else if(i->data < j->data)
+        {
+            i=i->next;
+        }
+        else
+        {
+            j=j->next;
+        }
+    }
+    return res->next;
+}
